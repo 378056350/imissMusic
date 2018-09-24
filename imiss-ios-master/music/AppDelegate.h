b38bdd6b5f305f631254d4e82f70a4c3861cf577
郑业强 <378056350@qq.com>
2018-09-24 21:56:21 +0800

    添加 iOS、Python 初始项目

---
+++ imiss-ios-master/music/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  music
+//
+//  Created by 郑业强 on 2018/9/24.
+//  Copyright © 2018年 kk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
