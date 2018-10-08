324e2daf8b39023b37e02711c25809125b7ecd07
郑业强 <378056350@qq.com>
2018-10-08 20:12:44 +0800

    rn - 删除rn项目，天杀的运行就够费时的，你妹的

--- imiss/src/redux/reducer/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import HomeRecommendReducer from './HomeRecommendReducer';
-
-const rootReducer = combineReducers({
-    HomeRecommendReducer: HomeRecommendReducer,
-});
-
-export default rootReducer;
-
