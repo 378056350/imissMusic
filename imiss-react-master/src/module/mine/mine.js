5aff958c36db4525af1cdab62a1afc279363f9e8
Yeqiang <15265296375@163.com>
2018-10-09 17:17:49 +0800

    react 继续编写代码

--- imiss-react-master/src/module/mine/mine.js
+++ imiss-react-master/src/module/mine/mine.js
@@ -1,5 +1,20 @@
 import '@CSS_PATH/mine.less'
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import Checkbox from '@material-ui/core/Checkbox';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import CommentIcon from '@material-ui/icons/Comment';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import Switch from '@material-ui/core/Switch';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import BluetoothIcon from '@material-ui/icons/Bluetooth';
 import { AdjustFont } from '@PUBLIC_PATH'
 import Header from '@COMPONENT_PATH/Header'
 
@@ -8,18 +23,115 @@ export default class mine extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      checked: ['wifi']
     };
   }
+
+  handleToggle = value => () => {
+    const { checked } = this.state;
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    this.setState({
+      checked: newChecked,
+    });
+  };
+
   
   // 主视图
   render() {
     return (
       <div className="kk_mine">
         <Header name={"AAAA"}/>
+        <Divider />
+        <List component="li" className={"list"}>
+          <ListItem button>
+            <ListItemText primary="Trash" />
+          </ListItem>
+          <ListItem button component="a" href="#simple-list">
+            <ListItemText primary="Spam" />
+          </ListItem>
+          <ListItem button component="a" href="#simple-list">
+            <ListItemText primary="Spam" />
+          </ListItem>
+          <ListItem button component="a" href="#simple-list">
+            <ListItemText primary="Spam" />
+            <ListItemIcon>
+              <BluetoothIcon />
+            </ListItemIcon>
+          </ListItem>
+          <ListItem button component="a" href="#simple-list">
+            <ListItemText primary="Spam" />
+          </ListItem>
+          <ListItem>
+            <ListItemIcon>
+              <BluetoothIcon />
+            </ListItemIcon>
+            <ListItemText primary="Bluetooth" />
+            <ListItemSecondaryAction>
+              <Switch
+                onChange={this.handleToggle('bluetooth')}
+                checked={this.state.checked.indexOf('bluetooth') !== -1}
+              />
+            </ListItemSecondaryAction>
+          </ListItem>
+        </List>
       </div>
     );
   }
 }
+
+
+// const styles = theme => ({
+//   root: {
+//     width: '100%',
+//     maxWidth: 360,
+//     backgroundColor: theme.palette.background.paper,
+//   },
+// });
+
+// function SimpleList(props) {
+//   const { classes } = props;
+//   return (
+//     // <div className={classes.root}>
+//     <div className="kk_mine">
+//       <List component="nav">
+//         <ListItem button>
+//           <ListItemIcon>
+//             <InboxIcon />
+//           </ListItemIcon>
+//           <ListItemText primary="Inbox" />
+//         </ListItem>
+//         <ListItem button>
+//           <ListItemIcon>
+//             <DraftsIcon />
+//           </ListItemIcon>
+//           <ListItemText primary="Drafts" />
+//         </ListItem>
+//       </List>
+//       <Divider />
+//       <List component="nav">
+//         <ListItem button>
+//           <ListItemText primary="Trash" />
+//         </ListItem>
+//         <ListItem button component="a" href="#simple-list">
+//           <ListItemText primary="Spam" />
+//         </ListItem>
+//       </List>
+//     </div>
+//   );
+// }
+
+// SimpleList.propTypes = {
+//   classes: PropTypes.object.isRequired,
+// };
+
+// export default withStyles(styles)(SimpleList);
 
 
